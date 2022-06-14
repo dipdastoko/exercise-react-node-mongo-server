c408833bbe8ad94e095c927390f0dfca461ef4d5
dipdastoko <dipdastoko@gmail.com>
2022-06-14 09:07:11 +0600

    delete complete

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+    const { id } = useParams();
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>{id}</h2>
         </div>
     );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
@@ -31,7 +32,7 @@ const Users = () => {
             <ul>
                 {
                     users.map(user => <li key={user._id}>{user.name} {user.email}
-                        <button>Update</button>
+                        <Link to={`users/update/${user._id}`}><button>Update</button></Link>
                         <button onClick={() => handleDeleteUser(user._id)}>X</button>
                     </li>)
                 }
