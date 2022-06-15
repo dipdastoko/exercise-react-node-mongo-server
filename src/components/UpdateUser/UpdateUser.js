a4af9d0274038cd5145470b9a34db3a8d8aa23a4
dipdastoko <dipdastoko@gmail.com>
2022-06-15 20:14:20 +0600

    update complete

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -8,20 +8,43 @@ const UpdateUser = () => {
         fetch(`http://localhost:5000/users/${id}`)
             .then(res => res.json())
             .then(data => setUser(data))
-    }, []);
+    }, [id]);
     const handleUpdateUser = e => {
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('Updated Successfully')
+                    setUser({});
+                }
+            })
         e.preventDefault();
-    }
+    };
     // update
     const handleNameChange = e => {
-
+        const updatedName = e.target.value;
+        const updatedUser = { name: updatedName, email: user.email };
+        setUser(updatedUser);
+    }
+    const handelEmailChange = e => {
+        const updatedEmail = e.target.value;
+        // const updatedUser = { ...user };
+        // updatedUser.email = updatedEmail;
+        const updateUser = { name: user.name, email: updatedEmail };
+        setUser(updateUser);
     }
     return (
         <div>
             <h2>Update {user.name}</h2>
             <form onSubmit={handleUpdateUser}>
                 <input type="text" onChange={handleNameChange} value={user.name || ''} />
-                <input type="email" value={user.email || ''} />
+                <input type="email" onChange={handelEmailChange} value={user.email || ''} />
                 <input type="submit" value="Update" />
             </form>
         </div>
