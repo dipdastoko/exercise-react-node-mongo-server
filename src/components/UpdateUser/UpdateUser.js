9c5ee996d29d5281f0890e22bede8bb6ff069777
dipdastoko <dipdastoko@gmail.com>
2022-06-15 19:02:08 +0600

    sldkfj

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+    const [user, setUser] = useState({});
     const { id } = useParams();
+    useEffect(() => {
+        fetch(`http://localhost:5000/users/${id}`)
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, []);
+    const handleUpdateUser = e => {
+        e.preventDefault();
+    }
+    // update
+    const handleNameChange = e => {
+
+    }
     return (
         <div>
-            <h2>{id}</h2>
+            <h2>Update {user.name}</h2>
+            <form onSubmit={handleUpdateUser}>
+                <input type="text" onChange={handleNameChange} value={user.name || ''} />
+                <input type="email" value={user.email || ''} />
+                <input type="submit" value="Update" />
+            </form>
         </div>
     );
 };
